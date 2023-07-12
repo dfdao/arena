567420ed348665771823692f73a2b6c5da70c7d1
cha0sg0d <0xcha0sg0d@gmail.com>
2023-07-11 17:56:28 -0700

    chore: channel id

--- server/bot.ts
+++ server/bot.ts
@@ -1,23 +1,31 @@
-import { Client, Events, GatewayIntentBits} from 'discord.js';
-import config from './config.json' assert { type: "json" };
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+import 'dotenv/config';
+
+const targetChannelId = '909812397680767006';
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
-const prefix = '!';
-
-client.on(Events.InteractionCreate, interaction => {
-  console.log(`interaction`, interaction)
+client.on(Events.InteractionCreate, (interaction) => {
+  console.log(`interaction`, interaction);
 });
 
 client.on(Events.ClientReady, () => {
   console.log(`Logged me in as ${client?.user?.tag}`);
 });
 
-client.on(Events.MessageCreate, async message => {
-  console.log(`CONTENT`, message.content)
-})
+client.on(Events.MessageCreate, async (message) => {
+  if (message.channelId !== targetChannelId) return;
+  if (message.content === '/verify') {
+    console.log(`verifiying ${message.author.username}`);
+    console.log(message);
+    message.channel.send(`verifying ${message.author.username}`);
+  }
+});
 
-
-client.login(config.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
